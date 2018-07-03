5eff67e600ee001bd9ddaa11a6e729d6931db4b6
Bishal Pun <bishalpun2013@gmail.com>
2018-07-03 18:08:46 +0545

    [IMP] website_form_recaptcha: remove gcaptcha from email

--- website_form_recaptcha/controllers/main.py
+++ website_form_recaptcha/controllers/main.py
@@ -1,6 +1,8 @@
 # Copyright 2016-2017 LasLabs Inc.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
+import logging
+
 from odoo import http
 from odoo.http import request
 from odoo.exceptions import ValidationError
@@ -8,6 +10,8 @@ from odoo.exceptions import ValidationError
 from odoo.addons.website_form.controllers.main import WebsiteForm
 
 import json
+
+_logger = logging.getLogger(__name__)
 
 
 class WebsiteForm(WebsiteForm):
@@ -29,6 +33,18 @@ class WebsiteForm(WebsiteForm):
 
     def extract_data(self, model, values):
         """ Inject ReCaptcha validation into pre-existing data extraction """
+        # delete recaptcha response code to make email clean
+        if 'g-recaptcha-response' in values:
+            del values["g-recaptcha-response"]
+
+        # Prepare and Prepend subject for the email
+        if 'form_type' in values:
+            if values['form_type'] == 'connect_form':
+                values['subject'] = values['Fullname'].title() + ' sent you ' + values['Subject'] + ' inquiry'
+            elif values['form_type'] == 'talk_to_our_expert_form':
+                values['subject'] = values['Fullname'].title() + ' wants to talk to our expert ' + values['Query for']
+
+            del values['form_type']
         res = super(WebsiteForm, self).extract_data(model, values)
         if model.website_form_recaptcha:
             captcha_obj = request.env['website.form.recaptcha']
