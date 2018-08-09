c1f94331e59e9ef37e13b4316509d9d138bb0680
Bishal Pun <bishalpun2013@gmail.com>
2018-08-09 21:46:54 +0545

    [IMP] website_footer_sticky: fix fixed position for footer

--- website_footer_sticky/static/src/js/website_footer_sticky.frontend.js
+++ website_footer_sticky/static/src/js/website_footer_sticky.frontend.js
@@ -5,16 +5,11 @@ odoo.define('website_footer_sticky.frontend', function(require) {
     "use strict";
 
     var base = require('web_editor.base');
-	console.log("La JS ma aayo hai!!!!!!!!!!!!!")
-	
+
     base.ready().then(function() {
-		console.log("La Function ma aayo hai!!!!!!!!!!!!!")
         var footer = $('footer');
         var div = $("div.footer");
-		console.log(footer);
-		console.log(div);
         if (div.attr('data-do-stick') === '1') {
-			console.log("La IF CONDITION ma aayo hai!!!!!!!!!!!!! 1")
             var div_clone = div.clone();
             div_clone.addClass('div-fixed-bottom');
             footer.append(div_clone);
